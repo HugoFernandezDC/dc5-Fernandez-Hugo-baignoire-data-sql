9c20517f113415886e7a3dc048254060e597ab80
HugoFernandezDC <78967037+HugoFernandezDC@users.noreply.github.com>
2023-11-07 16:07:16 +0100

    Ajout Exercice 8

---
+++ ex8.sql
@@ -0,0 +1,13 @@
+/*
+Trouver tous les officiers et intermédiaires qui ont une association :
+*/
+
+SELECT 
+    officer.name AS OfficerName, 
+    intermediary.name AS IntermediaryName 
+FROM 
+    assoc_inter_offi 
+JOIN 
+    officer ON assoc_inter_offi.officer = officer.id 
+JOIN 
+    intermediary ON assoc_inter_offi.inter = intermediary.id;
